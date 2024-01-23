df3b64ac18d7afd0bcf35c01ba09876ec2a50497
dipski <dipskii@protonmail.com>
2024-01-23 18:44:49 -0500

    uploading the entire .minecraft folder raw was stupid, use export instead! retard!

--- .minecraft/groovy/postInit/tinkers.groovy
+++
@@ -1,6 +0,0 @@
-crafting.remove('tconstruct:tools/pattern')
-crafting.addShaped('blank_pattern', item('tconstruct:pattern') * 1, [
-    [item('minecraft:paper'), item('minecraft:paper')],
-    [item('minecraft:paper'), item('minecraft:paper')]
-    ]
-)
